abf1557f493b7709f8c89ce68bee2bee0f3d53d3
Emil Forslund <emil.forslund@his.se>
2019-10-04 08:54:49 +0200

    Upload base for second assignment

---
+++ Deluppgift 2/deluppgift2.js
@@ -0,0 +1,29 @@
+// Välkommen till andra deluppgiften!
+//
+// Följ instruktionerna på kurssidan och ändra gärna i den här filen.
+// Du kan när som helst testa din kod genom att öppna 'index.html' i
+// en webbläsare, förslagsvis Chrome.
+//
+// Tips:
+// - 
+//
+// - 
+//
+// - 
+// 
+// - Om saker slutar fungera, försök kommentera ut ändringar tills 
+//   det fungerar igen.
+// 
+// - Eventuella fel syns om du högerklickar i webbläsaren och väljer 'Inspektera'.
+//   Du kan även skriva kod och evaluera direkt i konsollen där.
+//
+// - För att felsöka din kod, skriv loggmeddelanden med 'console.log("Testar...");'
+//
+// - Ibland används 'var' och ibland 'const'. Om du är osäker på vilken du
+//   skall använda så kör på 'var'.
+
+var gl;
+
+function main(context) {
+    gl = context;
+}
