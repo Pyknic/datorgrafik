98e88d964bd3569d5b83ba13f30d8442d10847f8
Emil Forslund <emil.forslund@his.se>
2019-10-22 15:53:41 +0200

    Create FPS Camera (using Rider)

--- Deluppgift 3/Assets/Scripts/CameraControl.cs
+++ Deluppgift 3/Assets/Scripts/CameraControl.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    private Camera myCamera;
+    public float mouseSpeed = 1.0f;
+    public float moveSpeed = 10.0f;
+    private Camera _myCamera;
     
     void Start()
     {
-        myCamera = GetComponent<Camera>();
+        _myCamera = GetComponent<Camera>();
     }
     
     void Update()
     {
-        
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+        var walkForward = Input.GetAxis("Vertical");
+        var walkRight   = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(mouseX) > float.Epsilon)
+        {
+            _myCamera.transform.Rotate(Vector3.up, mouseX * mouseSpeed, Space.World);
+        }
+            
+        if (Mathf.Abs(mouseY) > float.Epsilon)
+        {
+            _myCamera.transform.Rotate(Vector3.left, mouseY * mouseSpeed);
+        }
+
+        if (Mathf.Abs(walkForward) > float.Epsilon || Mathf.Abs(walkRight) > float.Epsilon)
+        {
+            var move = walkForward * Vector3.forward 
+                       + walkRight * Vector3.right;
+            
+            move = _myCamera.transform.localToWorldMatrix * move;
+            move -= Vector3.up * (Vector3.Dot(Vector3.up, move));
+            _myCamera.transform.localPosition += Time.deltaTime * moveSpeed * move.normalized;
+        }
     }
 }
