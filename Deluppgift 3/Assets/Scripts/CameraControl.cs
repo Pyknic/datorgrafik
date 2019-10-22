8f2eeb1314efc01905081efead08a50361960595
Emil Forslund <emil.forslund@his.se>
2019-10-22 14:36:16 +0200

    Create Deluppgift 3

---
+++ Deluppgift 3/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Camera myCamera;
+    
+    void Start()
+    {
+        myCamera = GetComponent<Camera>();
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Deluppgift 3/Assets/Scripts/RotateAxis.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAxis : MonoBehaviour
+{
+    public float rotateX;
+    public float rotateY;
+    public float rotateZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateX, rotateY, rotateZ);
+    }
+}
